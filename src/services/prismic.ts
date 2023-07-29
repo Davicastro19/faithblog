d893c987d801129b64e97bb32280ee13bcc130bd
DAVI CASTRO FERREIRA <48842971+Davicastro19@users.noreply.github.com>
2023-07-29 14:49:34 -0300

    Update prismic.ts

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,7 +2,7 @@ import * as prismic from '@prismicio/client'
 
 export const getPrismicClient = (config = {}) => {
   const client = prismic.createClient('https://blogfaith.cdn.prismic.io/api/v2', {
-   accessToken: 'MC5aSW5mWWhVQUFDa0EtbWZX.77-9F--_vWkych_vv71uee-_ve-_ve-_vQfvv70UKycR77-977-9QO-_vTbvv71V77-9N--_ve-_vXzvv70',
+   accessToken: '',
   });
 
   return client
